e0b4b14b438c5df7f3104426c58aebae45b3eee6
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2020-01-10 05:31:57 +0500

    Configured express app into REST API supplying mock data

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var ctrlApi = require('../controllers/api');
+
+router.get('/getSchedule', ctrlApi.getSchedule);
+
+router.post('/reserveSeat', ctrlApi.reserveSeat);
+
+router.post('/buySeat', ctrlApi.buySeat);
+
+router.post('/requestMeal', ctrlApi.requestMeal);
+
+router.post('/submitFeedback', ctrlApi.submitFeedback);
 
 module.exports = router;
